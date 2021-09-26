833d800497c47961ba34548c1c9b2825e2290356
SerpentSpirale <mathiasboulay@free.fr>
2021-09-26 19:32:09 +0200

    Remove dummy RerollableApp items

--- app/src/main/java/fr/spse/master_gacha_reroller/rerollable/RerollAppList.java
+++ app/src/main/java/fr/spse/master_gacha_reroller/rerollable/RerollAppList.java
@@ -17,22 +17,5 @@ public class RerollAppList {
     public static final List<BaseRerollableApp> rerollableAppList = new ArrayList<>();
     
     public static void initList(Context ctx){
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.konosuba", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.dsemu.drastic", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "skyline.emu", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.oasisfeng.greenify", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "org.telegram.messenger", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "org.schabi.newpipe", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "org.chromium.webapk.aOf39d6_v2", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.google.ar.core", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "us.zoom.videomeetings", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "nextapp.fx", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.hny", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.konosuba", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.,ghycx", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.fzeqgqe", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.u;ihk", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.fqyuku", R.string.com_nexon_konosuba));
-        rerollableAppList.add(new dummyRerollableApp( ctx, "com.nexon.feF", R.string.com_nexon_konosuba));
     }
 }
